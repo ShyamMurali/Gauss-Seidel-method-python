063b361ab4264eff45da78e8e7de926c40db33c0
Shyam Murali <59696444+ShyamMurali@users.noreply.github.com>
2021-03-18 13:45:28 +0530

    Update gauus-sidel.py

--- gauus-sidel.py
+++ gauus-sidel.py
@@ -2,12 +2,13 @@ print("consider eqn like ax+by+cx=d ")
 a1,b1,c1,d1=[ int(x) for x in input("coeff of exp1: ").split()]
 a2,b2,c2,d2=[ int(x) for x in input("coeff of exp2: ").split()]
 a3,b3,c3,d3=[ int(x) for x in input("coeff of exp3: ").split()]
+iterations=int(input("number of iterations: "))
 
 def process(a1,b1,c1,d1,a2,b2,c2,d2,a3,b3,c3,d3):
     x=0
     y=0
     z=0
-    for i in range(10):
+    for i in range(interations):
         x=(1/a1)*(d1-b1*y-c1*z)
         y=(1/b2)*(d2-a2*x-c2*z)
         z=(1/c3)*(d3 -a3*x-b3*y)
